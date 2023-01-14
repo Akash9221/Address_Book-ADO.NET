e25f028d648d051b606ecf7d7af19be250520f05
“akashmane3938” <maneakash3938@gmail.com>
2023-01-14 06:47:17 -0800

    [Akash]Add Data Into DB

---
+++ AddressBook Service/AddressBook Service/AddressBookService.cs
@@ -0,0 +1,54 @@
+﻿using DocuSign.eSign.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AddressBook_Service
+{
+    public class AddressBookService
+    {
+        public static string connectionString= @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Address_Book_SystemService_Database;";
+
+        public void AddAddressBookInDB(AddressBook address)
+        {
+            SqlConnection sQLConnection = new SqlConnection(connectionString);
+            try
+            {
+                using (sQLConnection)
+                {
+                    sQLConnection.Open();
+                    SqlCommand command = new SqlCommand("SPAddAddressBook", sQLConnection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@FirstName",address.FirstName);
+                    command.Parameters.AddWithValue("@LastName", address.LastName);
+                    command.Parameters.AddWithValue("@Address", address.Address);
+                    command.Parameters.AddWithValue("@City", address.City);
+                    command.Parameters.AddWithValue("@State", address.State);
+                    command.Parameters.AddWithValue("@Zip", address.Zip);
+                    command.Parameters.AddWithValue("@MobNo", address.MobNo);
+                    command.Parameters.AddWithValue("@Email", address.Email);
+                    command.Parameters.AddWithValue("@Type", address.Type);
+                    command.Parameters.AddWithValue("@AddressBookName", address.AddressBookName);
+                    int result = command.ExecuteNonQuery(); 
+                    sQLConnection.Close();  
+                    if(result>=1)
+                    {
+                      Console.WriteLine("Address Book Added Succesfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Data Found");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //Handle Exception Here
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+        
+
+    }
+    }
---
+++ AddressBook Service/AddressBook Service/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using DocuSign.eSign.Model;
+using System;
+
+namespace AddressBook_Service
+{
+    public class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            AddressBookService addressBook = new AddressBookService();
+            bool flag = true;
+            while(flag)
+            {
+                Console.WriteLine("Enter The Option= 1.AddAddressBook \n 2.RetrieveEntriesFromAddressBookDB \n 3.Update Data \n 4.Delete Data");
+                int option=Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+
+                        AddressBook address=new AddressBook()
+                        {
+                            FirstName = "Akash",
+                            LastName = "Mane",
+                            Address = "Bhigwan",
+                            City = "Pune",
+                            State = "Maha",
+                            Zip = 7865,
+                            MobNo = 724962454,
+                            Email = "maneakash3938@gmail.com",
+                            Type = "Novel",
+                            AddressBookName = "ABC",
+                        };
+                        addressBook.AddAddressBookInDB(address);
+                        break;
+
+                       
+                    case 4:
+                        addressBook.DeleteDataFromDatabase("Akash");
+                        break;
+                }
+            }
+        }
+    }
+}
